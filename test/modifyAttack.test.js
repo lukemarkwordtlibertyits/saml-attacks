ba84b02abc183c177a1b0a96a2ed9fe04e35ab4e
Luke Markwordt <luke.markwordt@libertyits.com>
2021-06-17 14:29:41 -0600

    anything?

--- test/modifyAttack.test.js
+++ test/modifyAttack.test.js
@@ -1,6 +1,6 @@
 require('jest')
 const fs = require('fs')
-const ModifyAttack = require('lib/modifyAttack')
+const ModifyAttack = require('../lib/modifyAttack')
 let xml_file
 
 beforeEach(() => {
@@ -13,33 +13,58 @@ describe('Happy Path', () => {
   beforeEach(() => {
     attribute_modify_file = fs.readFileSync('test/resources/attribute_modify.xml', 'utf8')
   })
-  it('Happy Path', () => {
-    const modified_saml = ModifyAttack(xml_file, 'uid', 'modified')
+  it('Happy Path', async () => {
+    const modified_saml = await ModifyAttack(xml_file, 'uid', 'modified')
     expect(modified_saml).toEqual(attribute_modify_file)
   })
 })
 
 describe('Invalid Input', () => {
-  it('Undefined Saml', () => {
-    expect(ModifyAttack(undefined, 'uid', 'modified')).toThrow('Invalid Saml Response')
+  it('Undefined Saml', async () => {
+    try {
+      await ModifyAttack(undefined, 'uid', 'modified')
+      fail('An error was expected')
+    } catch (error) {
+      expect(error).toEqual('Invalid Saml Response')
+    }
   })
 
-  it('Undefined Attribute', () => {
-    expect(ModifyAttack(xml_file, undefined, 'modified')).toThrow('Invalid Assertion Attribute')
+  it('Undefined Attribute', async () => {
+    try {
+      await ModifyAttack(xml_file, undefined, 'modified')
+      fail('An error was expected')
+    } catch (error) {
+      expect(error).toEqual('Invalid Assertion Attribute')
+    }
   })
 
-  it('Undefined Modified Field', () => {
-    expect(ModifyAttack(xml_file, 'uid', undefined)).toThrow('Invalid Modified Field')
+  it('Undefined Modified Field', async () => {
+    try {
+      await ModifyAttack(xml_file, 'uid', undefined)
+      fail('An error was expected')
+    } catch (error) {
+      expect(error).toEqual('Invalid Modified Field')
+    }
   })
 
-  it('Invalid Saml Response', () => {
+  it('Invalid Saml Response', async () => {
     const invalid_saml = fs.readFileSync('test/resources/invalid_saml.xml', 'utf8')
-    expect(ModifyAttack(invalid_saml, 'uid', 'modified')).toThrow('Invalid Saml Response')
+    try {
+      await ModifyAttack(invalid_saml, 'uid', 'modified')
+      fail('An error was expected')
+    } catch (error) {
+      expect(error).toEqual('Invalid Saml Response')
+    }
   })
 })
 
 describe('Corner Cases', () => {
-  it('Could not find Saml Attribute', () => {
-    expect(ModifyAttack(xml_file, 'miss', 'modified')).toThrow('Saml Attribute Miss')
+  it('Could not find Saml Attribute', async () => {
+    try {
+      await ModifyAttack(xml_file, 'miss', 'modified')
+      fail('An error was expected')
+    } catch (error) {
+      expect(error).toEqual('Saml Attribute Miss')
+    }
   })
 })
